11d293589604a2b0f1da565daf76b292c22fe1cd
Ali Barzegari dahaj <116592901+Ali-Barzegari-d@users.noreply.github.com>
2024-06-09 20:40:29 +0330

    Create Fibonacci.c

---
+++ Fibonacci/Fibonacci.c
@@ -0,0 +1,24 @@
+// Fibonacci Algorithm in C
+#include <stdio.h>
+int fib(int n)
+{
+	int a = 0, b = 1, c, i;
+	if (n == 0)
+		return a;
+	for (i = 2; i <= n; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
+}
+
+int main()
+{
+	int n = 9;
+	printf("%d", fib(n));
+	getchar();
+	return 0;
+}
+
+// This code is contributed by Ali Barzegari Dahaj
