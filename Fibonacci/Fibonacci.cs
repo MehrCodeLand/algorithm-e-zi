ecdd3ba26fe9ea37b3872db9de18e200eeb6ee05
Ali Barzegari dahaj <116592901+Ali-Barzegari-d@users.noreply.github.com>
2024-06-10 19:19:26 +0330

    Create Fibonacci.cs

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,30 @@
+//Fibonacci Algorithm in C#
+using System;
+
+namespace Fib {
+public class GFG {
+	static int Fib(int n)
+	{
+		int a = 0, b = 1, c = 0;
+
+		// To return the first Fibonacci number
+		if (n == 0)
+			return a;
+
+		for (int i = 2; i <= n; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		return b;
+	}
+	// main
+	public static void Main(string[] args)
+	{
+		int n = 9;
+		Console.Write("{0} ", Fib(n));
+	}
+}
+}
+
+// This code is contributed by Ali Barzegari Dahaj
