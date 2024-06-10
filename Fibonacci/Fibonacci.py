808c129bc13fef4b8f1202e21e1e2dead8469e32
Ali Barzegari dahaj <116592901+Ali-Barzegari-d@users.noreply.github.com>
2024-06-10 19:16:24 +0330

    Create Fibonacci.py

---
+++ Fibonacci/Fibonacci.py
@@ -0,0 +1,20 @@
+#Fibonacci Algorithm in Python
+def fibonacci(n):
+	a = 0
+	b = 1
+	if n < 0:
+		print("Incorrect input")
+	elif n == 0:
+		return a
+	elif n == 1:
+		return b
+	else:
+		for i in range(2, n+1):
+			c = a + b
+			a = b
+			b = c
+		return b
+
+print(fibonacci(9))
+
+# This code is contributed by Ali Barzegari Dahaj
